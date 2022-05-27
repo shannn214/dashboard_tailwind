207154d5ec03c0a55c4cee6aa4569538bd31e0ae
Shannn <gracejin214@gmail.com>
2022-05-27 18:22:11 +0800

    add sparkLine

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import {
+  ChartComponent,
+  SeriesDirective,
+  SeriesCollectionDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from '@syncfusion/ej2-react-charts'
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from '../../data/dummy'
 
-const Stacked = () => {
-  return <div>Stacked</div>
+const Stacked = ({ width, height }) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id="stack chart"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+    </ChartComponent>
+  )
 }
 
 export default Stacked
